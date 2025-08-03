892cf64b3853d4f6046ec922cb490e1ec778ed30
Jackie Wang <jaw039@ucsd.edu>
2025-08-03 15:48:09 -0700

    Fix city name interpolation in weather API request

--- weather.py
+++ weather.py
@@ -2,14 +2,15 @@ import requests
 
 ## get the city name from the enduser
 city = input("Which city's weather would you like to check? \n")
-response = requests.get("https://api.openweathermap.org/data/2.5/weather?q=city&appid=e1a450dc406ef1755ab4b44535e5de34")
+response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=e1a450dc406ef1755ab4b44535e5de34")
 # print(response.status_code)
 # print(response.text)
+
 
 ## stores the data into a dict
 API_Date = response.json()
 # print(API_Date)
-
+# print("City name from API:", API_Date["name"])
 ## gets the city weather condition
 print(API_Date["weather"][0]["main"])
 
