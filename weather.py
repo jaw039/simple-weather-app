371c0d11088ef9aa57543c6ebb611b94ef1f2d73
Jackie Wang <jaw039@ucsd.edu>
2025-08-03 20:18:23 -0700

    Add input validation and display sunrise/sunset times
    Introduces a loop to validate city input, prompting the user until a valid city is entered. Adds formatted output for weather condition, temperature, and sunrise/sunset times using the OpenWeatherMap API response.

--- weather.py
+++ weather.py
@@ -1,10 +1,17 @@
 import requests
+from datetime import datetime
 
-## get the city name from the enduser
-city = input("Which city's weather would you like to check? \n")
-response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=e1a450dc406ef1755ab4b44535e5de34")
-# print(response.status_code)
-# print(response.text)
+while True:
+    ## get the city name from the enduser
+    city = input("Which city's weather would you like to check? \n")
+
+    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=e1a450dc406ef1755ab4b44535e5de34")
+    # print(response.status_code)
+    # print(response.text)
+    if response.status_code == 200:
+        break
+    else:
+        print("Invalid City Name")
 
 
 ## stores the data into a dict
@@ -12,20 +19,33 @@ API_Date = response.json()
 # print(API_Date)
 # print("City name from API:", API_Date["name"])
 ## gets the city weather condition
-print(API_Date["weather"][0]["main"])
+weather_condition = (API_Date["weather"][0]["main"])
 
 ## get the weather temp (fahrenheit)
 class KelvinToCelciusConverter:
     def convert(self, kelvin):
-        return (kelvin - 273.15) * 9 / 5 + 32
+        return round((kelvin - 273.15) * 9 / 5 + 32)
 
 converter = KelvinToCelciusConverter()
 kelvin_value = (API_Date["main"]["temp"]) 
-print(converter.convert(kelvin_value))
+weather_temp = str(converter.convert(kelvin_value))
 
 ## if there is rain prints the rain status, else print no rain
 if "rain" in API_Date and "1h" in API_Date["rain"]:
-    print(API_Date["rain"]["1h"])
+    print(" rain " + API_Date["rain"]["1h"])
 else:
-    print("No Rain Today")
+    print("No Rain Currently")
 
+## sunrise and sunset time
+sun_rise_timestamp = datetime.fromtimestamp(API_Date["sys"]["sunrise"])
+weather_sunrise = sun_rise_timestamp.strftime("%I:%M %p")
+sun_set_timestamp = datetime.fromtimestamp(API_Date["sys"]["sunset"])
+weather_sunset = sun_set_timestamp.strftime("%I:%M %p")
+
+print("The weather in " + city + " is " + weather_condition + " with a temperature of " + weather_temp
+      + "°F.")
+print("Sunrise is at " + weather_sunrise + "\nSunset is at " + weather_sunset)
+
+
+
+# Input validation loop - If a city isn't found, ask the user to try again instead of crashing
